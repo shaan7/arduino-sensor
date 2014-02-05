b9b095e047849286cfd678749504739dfae551fd
Shantanu Tushar <shantant@thoughtworks.com>
2014-02-05 12:03:00 +0530

    Include the Arduino sketch and update README

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
 arduino-sensor
 ==============
 
-Arduino code that allows remote GPIO configuration over nrf24l01 wireless modules
+Arduino code that allows remote GPIO configuration over nrf24l01 wireless modules.
+The setup uses two nrf24l01 modules-
+
+* One on the raspberry pi
+* One on the arduino which is running this code.
+
+To setup the nrf24l01 module, use instructions at http://conoroneill.net/arduino-and-raspberry-pi-communicating-over-2-4ghz-with-cheap-nrf24l01-modules/
+
+For sending commands from Pi to Arduino wirelessly, modify and use one of the examples in https://github.com/stanleyseow/RF24/tree/master/librf24-rpi/librf24/examples
+
+Command Syntax
+==============
+
+Initial configuration
+-------------
+
+Once the arduino is up and running this code, it starts waiting for configuration.
+Send it the following command to configure-
+
+    list_of_input_pins;list_of_output_pins
+
+For example-
+
+    2,3;4,5
+
+means that make pins 2 and 3 as input and make 4 and 5 output.
+
+Once configured, this code sends out the values on the input pins over the nrf24l01 channel.
+
+Setting output pins
+-------------
+
+Once configured, you can send it a command in this syntax to set values on output pins-
+
+    2,1:3,0
+
+This will make pin 2 HIGH and pin 3 LOW.
+
+Note that,
+
+* The pins must be one of the pins that have been in the list of output pins while configuring
+* If you want to use Analog pins, make sure to use 14 for A0, 15 for A1 and so on.
+
+Thanks To
+==============
+
+* RF24 library from https://github.com/stanleyseow/RF24
+* Awesome setup instructions from http://conoroneill.net/arduino-and-raspberry-pi-communicating-over-2-4ghz-with-cheap-nrf24l01-modules/#comment-5997
+
---
+++ arduino_nrf24l01.ino
@@ -0,0 +1,220 @@
+#include <RF24.h>
+
+/*
+This attribution is specified as a lack of license in the original code.
+If you are the original author, please contact and make the license explicit.
+
+With code fragments by Stanley Seow's RF24
+https://github.com/stanleyseow/RF24/
+*/
+
+#include <SPI.h>
+#include "printf.h"
+
+#define RF_SETUP 0x17
+
+// Set up nRF24L01 radio on SPI pin for CE, CSN
+RF24 radio(8,9);
+
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0x7365727631LL };
+
+boolean isConfigured = false;
+
+uint8_t *inputPins = NULL;
+uint8_t *outputPins = NULL;
+
+static const int BUF_MAX = 32;
+static const uint8_t INVALID = 255;
+
+void setup(void)
+{
+  Serial.begin(57600);
+
+  printf_begin();
+
+  radio.begin();
+
+  // Enable this seems to work better
+  radio.enableDynamicPayloads();
+
+  radio.setDataRate(RF24_1MBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(76);
+  radio.setRetries(15,15);
+
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+
+  // Dump the configuration of the rf unit for debugging
+  radio.printDetails();
+  delay(500);
+
+  radio.startListening();
+}
+
+void loop(void)
+{
+  while (!isConfigured) {
+    listenForConfiguration();
+  }
+
+  if (outputPins) {
+    receiveAndApplyOutputs(outputPins);
+  }
+
+  if (inputPins) {
+    readSensorInputs(inputPins);
+  }
+
+  delay(100);
+}
+
+void readSensorInputs(uint8_t *pins)
+{
+  char buffer[BUF_MAX]="";
+
+  for (int i=0; pins[i]!=INVALID; i++) {
+    const uint8_t pin = pins[i];
+    uint8_t value = pin > 13 ? analogRead(pin) : digitalRead(pin);
+
+    char str[BUF_MAX]="";
+    sprintf(str, "%d,%d;", pin, value);
+    strcat(buffer, str);
+  }
+
+  if (strlen(buffer) == 0)
+    return;
+
+  radio.stopListening();
+  delay(20);
+
+  if ( radio.write( buffer, strlen(buffer)) ) {
+    printf("SENT %s\n", buffer);
+  }
+
+  radio.startListening();
+  delay(20);
+}
+
+void applyValueToPin(char *pinAndValue, uint8_t *pins)
+{
+  char *pin = strsep(&pinAndValue, ",");
+  char *value = strsep(&pinAndValue, ",");
+
+  uint8_t pinNum = strtol(pin, NULL,  10);
+  uint8_t valueNum = strtol(value, NULL,  10);
+
+  boolean found = false;
+  for (int i=0; pins[i]!=INVALID; i++) {
+    if (pinNum == pins[i]) {
+      found = true;
+      break;
+    }
+  }
+
+  if (found) {
+    printf("SET PIN %d to %d\n", pinNum, valueNum);
+    digitalWrite(pinNum, valueNum == 0 ? LOW : HIGH);
+  } else {
+    printf("WARNING: Attempt to set PIN %d when not set to Output\n", pinNum);
+  }
+}
+
+void receiveAndApplyOutputs(uint8_t *pins)
+{
+  while (radio.available()) {
+    uint8_t length = radio.getDynamicPayloadSize();
+    char *buffer = (char*)malloc(length);
+
+    radio.read(buffer, length);
+    buffer[length-1] = '\0';
+
+    char *pinAndValue = NULL;
+    while ((pinAndValue = strsep(&buffer, ":")) != NULL) {
+      applyValueToPin(pinAndValue, pins);
+      free(pinAndValue);
+    }
+  }
+}
+
+void listenForConfiguration()
+{
+  delay(20);
+
+  printf("Waiting for configuration...\n");
+  while ( !radio.available())
+    ;
+
+  uint8_t length = radio.getDynamicPayloadSize();
+  char *buffer = (char*)malloc(length);
+
+  radio.read(buffer, length);
+  buffer[length-1] = '\0';
+
+  if (strlen(buffer)) {
+    printf("Received Configuration of %d bytes\n", length);
+    printf("%s\n", buffer);
+    isConfigured = applyConfiguration(buffer);
+  } else {
+    printf("Warning: Got empty configuration\n");
+  }
+
+  free(buffer);
+}
+
+boolean applyConfiguration(char *config)
+{
+  boolean didApply = false;
+  char *input = strsep(&config, ";");
+  char *output = strsep(&config, ";");
+
+  printf("INPUT %s\n", input);
+  printf("OUTPUT %s\n", output);
+
+  if (strlen(input)) {
+    inputPins = parsePins(input);
+    configurePins(inputPins, INPUT);
+    didApply = true;
+  }
+  if (strlen(output)) {
+    outputPins = parsePins(output);
+    configurePins(outputPins, OUTPUT);
+    didApply = true;
+  }
+
+  return didApply;
+}
+
+void configurePins(uint8_t *pins, uint8_t mode)
+{
+  const char *modeString = mode ? "OUTPUT" : "INPUT";
+
+  for (int i=0; pins[i]!=INVALID; i++) {
+    const uint8_t pin = pins[i];
+    printf("Enabling %s on %d\n", modeString, pin);
+    pinMode(pin, mode);
+  }
+}
+
+uint8_t* parsePins(char *pins)
+{
+  char *pin;
+
+  //One comma means two numbers, so +1 int
+  //And another int to store INVALID to denote end
+  int numCommas = 2;
+  for (int i=0; pins[i]; i++)
+    numCommas = pins[i]==',' ? numCommas + 1 : numCommas;
+
+  uint8_t *pinArray = (uint8_t*) malloc(numCommas*sizeof(uint8_t));
+
+  int i=0;
+  while ((pin = strsep(&pins, ",")) != NULL) {
+    uint8_t pinNum = strtol(pin, NULL,  10);
+    pinArray[i++] = pinNum;
+  }
+
+  pinArray[i] = INVALID;  //Denotes the end of array
+  return pinArray;
+}
+
